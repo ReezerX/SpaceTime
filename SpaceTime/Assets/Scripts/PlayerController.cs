680ac64276840bee5d2079aac88af88150cfc01a
Dunixed <43297937+Dunixed@users.noreply.github.com>
2020-06-07 17:43:47 +0200

    Add files via upload

--- SpaceTime/Assets/Scripts/PlayerController.cs
+++ SpaceTime/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
 public float moveSpeed = 5f;
 public Transform movePoint;
 
+    public LayerMask movementSTOP;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,17 +22,22 @@ public Transform movePoint;
     {
 
     	transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
-    	
-       if (Mathf.Abs(Input.GetAxisRaw("horuizontal")) == 1f){
-
-            movePoint.position += new Vector3(Input.GetAxisRaw("horuizontal"), 0f, 0f);
-
-       	}; 
-
-       if (Mathf.Abs(Input.GetAxisRaw("vertical")) == 1f)
+    	//movement
+        if(Vector3.Distance(transform.position, movePoint.position) <= .05f) { 
+            //lef - right
+       if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f){
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, movementSTOP)) { 
+            movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                };
+            } 
+            //up - down
+       else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
-
-       	movePoint.position += new Vector3(0f, Input.GetAxisRaw("vertical"), 0f);
-        }; 
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Vertical"), 0f, 0f), .2f, movementSTOP))
+                {
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                };
+            };
+        };
     }
 }
