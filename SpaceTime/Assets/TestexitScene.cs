389f1c0e80243f52c74cc7ea5d1bd8b55f7f6090
Dunixed <43297937+Dunixed@users.noreply.github.com>
2020-07-06 20:02:27 +0200

    Test combat scene exit
    Added test button for exiting combat scene to prototype scene.

---
+++ SpaceTime/Assets/TestexitScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TestexitScene : MonoBehaviour
+{
+    public string levelName;
+
+    public Button yourButton;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+    void TaskOnClick()
+    { 
+        //loading level with build string
+        SceneManager.LoadScene(levelName);
+    }
+           
+}
